de3940e428d3acea22079a46ea235acb0acafd8b
cheney <516396859@qq.com>
2026-07-27 22:47:40 +0800

    style(knowledge): 更新空状态的文档图标
    替换了原有的文档图标为更贴合知识库场景的新图标，统一空状态的视觉展示

--- src/views/knowledge/components/KbMainContent.vue
+++ src/views/knowledge/components/KbMainContent.vue
@@ -224,8 +224,8 @@
     </div>
     <div class="empty-state" v-else>
       <svg width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1">
-        <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
-        <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
+        <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+        <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
       </svg>
       <h2>选择一个知识库</h2>
       <p>从左侧选择或创建一个知识库开始</p>
